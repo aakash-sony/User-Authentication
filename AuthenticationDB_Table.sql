f2eae5de675f1dbc0da8b796540b8e52f2f11509
Akash Soni <157835381+aakash-sony@users.noreply.github.com>
2024-08-26 00:25:58 +0530

    Add files via upload

---
+++ AuthenticationDB_Table.sql
@@ -0,0 +1,10 @@
+create table UserReg65(
+    uName VARCHAR2(20),
+    pWord VARCHAR2(15),
+    fName VARCHAR2(15),
+    lName VARCHAR2(15),
+    addr varchar2(20),
+    mId varchar2(25),
+    phone number(15),
+    primary Key(uName, pWord)
+);
